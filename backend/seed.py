838fc024a8587b9012772dedc06dc32dbbd56da5
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-05-03 11:47:20 -0700

    update seed file w new db structure

--- backend/seed.py
+++ backend/seed.py
@@ -10,17 +10,25 @@ now = datetime.datetime.now()
 db.drop_all()
 db.create_all()
 
-image1 = Image(date_time_created=now,
-               gps_latitude="40.76",
-               gps_longitude="40.76",
-               make="EASTMAN KODAK COMPANY",
-               model="KODAK CX7530 ZOOM DIGITAL CAMERA")
+image1 = Image(
+    date_time_created=now,
+    date_time_uploaded=now,
+    gps_latitude="40.76",
+    gps_longitude="40.76",
+    make="EASTMAN KODAK COMPANY",
+    model="KODAK CX7530 ZOOM DIGITAL CAMERA",
+    path="img.jpeg",
+    caption="I am a test caption")
 
-image2 = Image(date_time_created=now,
-               gps_latitude="40.76",
-               gps_longitude="40.76",
-               make="FUJIFILM",
-               model="FinePix6900ZOOM")
+image2 = Image(
+    date_time_created=now,
+    date_time_uploaded=now,
+    gps_latitude="40.76",
+    gps_longitude="40.76",
+    make="FUJIFILM",
+    model="FinePix6900ZOOM",
+    path="img2.jpeg",
+    caption="I am not a test caption")
 
 db.session.add_all([image1, image2])
 db.session.commit()
