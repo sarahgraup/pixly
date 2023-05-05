edcc18dc24efa1987931e0e8ada4630c0de86ad6
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-05-04 20:05:33 -0700

    create imagepage component

--- frontend/src/images/ImagePage.js
+++ frontend/src/images/ImagePage.js
@@ -1,7 +1,31 @@
-function ImagePage({images}){
-    return(
-        <p>image page</p>
-    )
+import { Navigate, useParams } from "react-router";
+import ImageCard from "./ImageCard";
+import ExifData from "./ExifData";
+
+/**
+ * Display component for image page
+ * props
+ *  - images [{image_data, url},...]
+ */
+function ImagePage({ images }) {
+    console.log("inside ImagePage");
+    console.log("images=", images)
+    const { id } = useParams();
+    console.log("id=", id);
+    const image = images.find(img => +img.image_data.id === +id);
+    if (!image){
+        return <Navigate to="/gallery"/>
+    }
+
+    // const image = images.filter(img => +img.image_data.id === +id)[0];
+    console.log("image=", image);
+    return (
+        <div className="ImagePage">
+            <ImageCard image={image} />
+            <ExifData image={image}/>
+        </div>
+
+    );
 
 }
 export default ImagePage;
